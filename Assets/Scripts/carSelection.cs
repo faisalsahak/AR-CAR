0a17f069b50fff059c52805612f4a0e8f7b64ccd
user <user@users-mbp.public.burnaby.ca>
2019-05-22 11:25:04 -0700

    added the buttons to the canvas and wrote the script for the buttons to change the cars and loop through them

---
+++ Assets/Scripts/carSelection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSelection : MonoBehaviour{
+
+	//we create an empty list for the gameObjects
+	private GameObject[] carList;
+  private int currentCar = 0;
+  // Start is called before the first frame update
+  void Start(){
+		//count the child game objects from the cars transform
+  	carList = new GameObject[transform.childCount];
+  	//loop through the child items and put them in the carlist array
+  	for(int i = 0; i <transform.childCount; i++){
+  		carList[i] = transform.GetChild(i).gameObject;
+  	}
+
+  	//Deactivates all the game objects in the list
+  	foreach(GameObject gameObject in carList){
+  		gameObject.SetActive(false);
+  	}
+  	//activates the first one
+  	if(carList[0]){
+  		carList[0].SetActive(true);
+  	}
+      
+  }
+
+  public void toggleCars(string direction){
+    carList[currentCar].SetActive(false);
+
+    if(direction == "Right"){
+      currentCar++;
+      if(currentCar>=carList.Length)
+        currentCar = 0;
+    }else if(direction == "Left"){
+      currentCar--;
+      if(currentCar < 0)
+        currentCar = carList.Length -1;
+    }
+
+    carList[currentCar].SetActive(true);
+  }
+}
